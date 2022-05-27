b644d6f0e4593570144ddc46fe9a64da1581a8e1
Dary <dary.cc@gmail.com>
2022-05-27 05:37:52 -0500

    refatored

--- Exercises/image-corousel/js.js
+++ Exercises/image-corousel/js.js
@@ -1,24 +1,25 @@
-var images = [
-  "Photos/1.jpg",
-  "Photos/2.jpg",
-  "Photos/3.jpg",
-  "Photos/4.jpg",
-  "Photos/5.jpg",
-  "Photos/6.jpg",
-  "Photos/7.jpg",
+const images = [
+  'Photos/1.jpg',
+  'Photos/2.jpg',
+  'Photos/3.jpg',
+  'Photos/4.jpg',
+  'Photos/5.jpg',
+  'Photos/6.jpg',
+  'Photos/7.jpg',
 ];
-var imagenes= document.querySelector('img');
-var index= 0;
-var next = document.getElementById('next-button');
+const imagenes= document.querySelector('img');
+let index= 0;
+const next = document.getElementById('next-button');
 
-var incrementIndex = function() {
-  if (index < images.length - 1)
+const incrementIndex = function() {
+  if (index < images.length - 1) {
     index++;
-  else
+  } else {
     index = 0;
+  };
 
   return index;
-}
-next.onclick = function(){
-  imagenes.setAttribute('src',images[incrementIndex(index)]);
-}
+};
+next.onclick = function() {
+  imagenes.setAttribute('src', images[incrementIndex(index)]);
+};
--- JS/async/prueba5.js
+++ JS/async/prueba5.js
@@ -4,7 +4,7 @@ let stocks = {
     holder : ["cone", "cup", "stick"],
     toppings : ["chocolate", "peanuts"],
  };
-let is_shop_open= true; 
+let is_shop_open= true;
 let order = ( time, work ) => {
 
   return new Promise( ( resolve, reject )=>{
